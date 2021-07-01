226b3290afec13e592fd8319ed9f61bb905d2fe6
CHOI DAE GEON <chleorjs37@gmail.com>
2021-07-01 10:59:57 +0900

    webpack.config.js devServe 세팅

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 module.exports = {
     mode : 'development',
+    target : ['web', 'es5'],
     resolve:{ //확장자를 설정함으로써 웹팩이 알아서 해석하도록 세팅
         extensions :['.js','.jsx']
     },
@@ -16,7 +17,14 @@ module.exports = {
                 use:{
                     loader : "babel-loader",
                     options:{
-                        presets: ["@babel/preset-env",'@babel/preset-react'],
+                        presets: [
+                            ['@babel/preset-env', {
+                                targets :{
+                                    browsers : ['> 1% in  KR']
+                                },
+                                debug: true,
+                            }]
+                            ,'@babel/preset-react'], //JSX로 만든 코드들을 변환
                     }
                 }
             }
@@ -27,4 +35,11 @@ module.exports = {
         filename: "app.js", // 아웃풋 시킬 파일명
         path: path.join(__dirname,'dist'),  // 아웃풋 할 폴더
     },
+    devServer:{
+        publicPath: '/dist/', //devServer경로
+        hot:true, //화면이 변경 될때 전체 파일 로드 하는게 아니라 한개만 로드
+        overlay: true,//에러를 브라우저에 표시
+        stats:'errors-only', //에러 표시 기준
+        historyApiFallback: true, // 무조건 index.html로 향하도록
+    }
 }
